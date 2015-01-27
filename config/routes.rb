285050b3f7e0d63bacdc6d8cdbfbfdcd46871134
jykke <jyri.lehva@saunalahti.fi>
2015-01-27 22:55:00 +0200

    setupping sessions

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   resources :ratings, only: [:index, :new, :create, :destroy]
 
+  resource :session, only: [:new, :create, :delete]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
