ecb2a641f5da8e0951cf28ce3552a75d2cf7e646
jykke <jyri.lehva@saunalahti.fi>
2015-01-29 19:53:02 +0200

    associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   include RatingAverage
 
   has_many :ratings
+  has_many :beers, through: :ratings
 
   validates :username, uniqueness: true
   validates :username, length: {
