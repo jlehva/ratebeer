42e6b09b79c64a14c655f4e4be25d09b720297db
jykke <jyri.lehva@saunalahti.fi>
2015-01-29 19:11:16 +0200

    week 3, exercise 5

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,6 +4,12 @@ class Brewery < ActiveRecord::Base
 
   include RatingAverage
 
+  validates :year, numericality: {
+                     only_integer: true,
+                     greater_than_or_equal_to: 1042,
+                     less_than_or_equal_to: 2015
+                 }
+
   def restart
     self.year = 2015
     puts "changed year to #{year}"
